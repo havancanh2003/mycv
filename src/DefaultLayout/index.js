4d04c114082f9332876fece6d7927a9a14a9871b
havancanh2003 <havancanh2003@gmail.com>
2023-10-09 19:41:48 +0700

    responsive

--- src/DefaultLayout/index.js
+++ src/DefaultLayout/index.js
@@ -1,10 +1,16 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import classNames from "classnames/bind";
+import style from "./DefaultLayout.module.scss";
+const cx = classNames.bind(style);
 function DefaultLayout({ children }) {
   return (
     <div>
       <Header />
-      {children}
+      <div className={cx("layout")}>
+        <div className={cx("pc")}>{children}</div>
+        <div className={cx("mob")}>Sorry, MyCv không hỗ trợ trên mobile</div>
+      </div>
       <Footer />
     </div>
   );
